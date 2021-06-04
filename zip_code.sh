c85f3ad232c335b346e5cad14e9496eb19e2a8a5
Michael Zhang <mzio@users.noreply.github.com>
2021-06-04 02:33:22 -0700

    Update zip_code.sh

--- zip_code.sh
+++ zip_code.sh
@@ -7,5 +7,5 @@ cd ..
 rm -f neurips_code_submission.zip
 rm -rf neurips_code_submission/
 rsync -avzh --exclude .git/ --exclude .ipynb_checkpoints/ --exclude sandbox/ --exclude "*.sh" --exclude .DS_Store --exclude .gitignore --exclude __pycache__/ \
-  slice-and-dice-smol/ neurips_code_submission/
+  correct-and-contrast/ neurips_code_submission/
 zip -r neurips_code_submission.zip neurips_code_submission/
